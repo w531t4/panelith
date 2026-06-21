6347876cf1501e152ad238975b9700e8f11bc7f2
Aaron White <w531t4@gmail.com>
2026-06-21 14:16:52 -0400

    remove stale stuff from repo

--- tools/gerbfix5.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# fix for stray "%" in gerbers
-# G04 Created by KiCad (PCBNEW 5.0.0-rc2+dfsg1-2) date Wed Jun  6 21:24:22 2018*
-
-for file in plot/*.g[btm]?
-do
-  sed -i -f tools/gerbfix5.sed $file
-done
